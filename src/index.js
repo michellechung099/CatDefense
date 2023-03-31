60772f318b670eb3feeb32f3c647550286525b47
michelle.chung <mchung099@gmail.com>
2023-03-31 16:01:24 -0700

    canvas and waypoints

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import Enemy from "./enemy";
+// import Enemy from "./enemy";
+import Waypoints from "./waypoints.js"
 
-const canvas = document.getElementById('game-canvas');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+document.addEventListener("DOMContentLoaded", function() {
+  const canvas = document.getElementById("game-canvas");
+  const c = canvas.getContext("2d");
 
-const img = new Image();
-img.src = 'assets/gameMap.png';
-img.onload = () => {
-  ctx.drawImage();
-}
+  canvas.width = 1280;
+  canvas.height = 768;
+
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  const img = new Image();
+  img.onload = () => {
+    c.drawImage(img, 0, 0)
+  }
+  img.src= "assets/finalMap.png"
+
+});
